54a9f28a5b60357282acdc4602eb7dd57a93cd7f
FelipeJunior20 <juniorxbox2020@gmail.com>
2025-07-29 10:38:34 -0300

    organização projeto

---
+++ src/app/Adotantes.java
@@ -0,0 +1,45 @@
+package app;
+
+public class Adotantes {
+    
+    private String Cpf;
+    private String Nomecompleto;
+    private String Endereço;
+    private String Telefone;
+
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String Cpf) {
+        this.Cpf = Cpf;
+    }
+
+    public String getNomecompleto() {
+        return Nomecompleto;
+    }
+
+    public void setNomecompleto(String Nomecompleto) {
+        this.Nomecompleto = Nomecompleto;
+    }
+
+    public String getEndereço() {
+        return Endereço;
+    }
+
+    public void setEndereço(String Endereço) {
+        this.Endereço = Endereço;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+
+
+}
+
---
+++ src/app/Gatos.java
@@ -0,0 +1,123 @@
+package app;
+
+import java.util.Date;
+
+public class Gatos {
+    
+    private String Cod;
+    private String Nome;
+    private String Raça;
+    private String Apelido;
+    private String Sexo;
+    private Date datadeEntrada;
+    private int idadeAproximada;
+    private String nomedaRação;
+    private double quantidadedaRação;
+    private double Peso;
+    private Date datadeSaida;
+    private String CpfAdotante;
+
+    public String getCod() {
+        return Cod;
+    }
+
+    public void setCod(String Cod) {
+        this.Cod = Cod;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getRaça() {
+        return Raça;
+    }
+
+    public void setRaça(String Raça) {
+        this.Raça = Raça;
+    }
+
+    public String getApelido() {
+        return Apelido;
+    }
+
+    public void setApelido(String Apelido) {
+        this.Apelido = Apelido;
+    }
+    
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    public Date getdatadeEntrada() {
+        return datadeEntrada;
+    }
+
+    public void setDatadeEntrada(Date datadeEntrada) {
+        this.datadeEntrada = datadeEntrada;
+    }
+
+    public int getIdadeAproximada() {
+        return idadeAproximada;
+    }
+
+    public void setIdadeAproximada(int idadeAproximada) {
+        this.idadeAproximada = idadeAproximada;
+    }
+
+    public String getNomedaRação() {
+        return nomedaRação;
+    }
+
+    public void setNomedaRação(String nomedaRação) {
+        this.nomedaRação = nomedaRação;
+    }
+
+    public double getQuantidadedaRação() {
+        return quantidadedaRação;
+    }
+
+    public void setQuantidadedaRação(double quantidadedaRação) {
+        this.quantidadedaRação = quantidadedaRação;
+    }
+
+    public double getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(double Peso) {
+        this.Peso = Peso;
+    }
+
+    public Date getDatadeSaida() {
+        return datadeSaida;
+    }
+
+    public void setDatadeSaida(Date datadeSaida) {
+        this.datadeSaida = datadeSaida;
+    }
+
+    public String getCpfAdotante() {
+        return CpfAdotante;
+    }
+
+    public void setCpfAdotante(String CpfAdotante) {
+        this.CpfAdotante = CpfAdotante;
+    }
+
+    
+
+
+
+
+
+
+}
